b046656b0d4386aff8179b066ee6890d541e03ed
Anders Leung <anders_leung@hotmail.com>
2016-06-21 17:04:03 -0400

    Before responsive design
    Content and styles and javascripts.

---
+++ public/javascripts/index.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Anders on 2016-06-20.
+ */
+$(document).ready(function() {
+    $('.corner-info').hide();
+
+    $('#carousel-button').on('click', function() {
+        $('#carousel').slideToggle();
+    });
+
+    $('.panel').hover(
+        function() {
+            $(this).css('border', '#cee0ee solid 1px');
+            $(this).find('.corner-info').show();
+        }, function() {
+            $(this).css('border', '0');
+            $(this).find('.corner-info').hide();
+        });
+});
